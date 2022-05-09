db475966d19d8b06c059587f2545ece60032b91b
Sagar Malik <52121616+sagar2582@users.noreply.github.com>
2022-05-09 17:32:08 +0530

    init

---
+++ isAprime.py
@@ -0,0 +1,11 @@
+x = 11
+if x > 1:
+    
+    for i in range(2, int(x/2)+1):
+        if (x % i == 0):
+            print("It's not a prime")
+            break
+    else:
+        print("It's a prime")
+else:
+    print("It's not a prime")
