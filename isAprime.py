c5326f0df5b648618ef2ca388323cd1a08cfaaba
Sagar Malik <52121616+sagar2582@users.noreply.github.com>
2022-05-10 15:22:11 +0530

    update

--- isAprime.py
+++ isAprime.py
@@ -1,7 +1,7 @@
 x = 11
 if x > 1:
     
-    for i in range(2, int(x/2)+1):
+    for i in range(2, int(x**0.5)+1):
         if (x % i == 0):
             print("It's not a prime")
             break
