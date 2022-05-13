10c139ce20858a07a099579d34c366d8cfd7cc1d
Sagar Malik <52121616+sagar2582@users.noreply.github.com>
2022-05-13 21:37:41 +0530

    update

--- nth_fibonacci_number.py
+++ nth_fibonacci_number.py
@@ -1,11 +1,11 @@
 def fibo(n):
-    if n <= 0:
+    if n < 0:
         print("Incorrect output")
-    elif n == 1:
+    elif n == 0:
         return 0
-    elif n == 2:
+    elif n == 1:
         return 1
     else:
         return fibo(n-1)+fibo(n-2)
 
-print(fibo(10))
+print(fibo(12))
